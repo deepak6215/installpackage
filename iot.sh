8b4979e1a6ec095fd9bf70cb077248f836e52531
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-01-04 19:05:41 +0530

    Update iot.sh

--- iot.sh
+++ iot.sh
@@ -34,38 +34,6 @@ sudo apt-get install openssh-client -y
 sudo apt-get install ntp -y
 sudo apt install curl -y
 
-mkdir -p /var/www/csvbackup
-mkdir -p /var/www/report/export/Scheduled_Report
-mkdir -p /var/www/iSolar/fetchDDT/csv
-mkdir -p /var/log/scaback_csv_influx_ingestor
-mkdir -p /var/log/prometheus
-mkdir -p /var/log/iGate_log
-mkdir -p /home/iplonshare/Scheduled_Report
-mkdir -p /home/iplonftp/csvbackup
-mkdir -p /home/iplonftp/Scheduled_Report
-
-adduser iplon www-data
-
-chown www-data:www-data -R /var/www/*
-chown www-data:www-data -R /var/www/.config/
-
-chmod 755 -R /var/www/*
-chmod 755 -R /var/www/.config/
-chmod 777 -R /var/www/report/export/Scheduled_Report/
-chmod 777 -R /var/www/iSolar/fetchDDT/csv
-
-chown root:crontab /var/spool/cron/crontabs/root
-chmod 600 /var/spool/cron/crontabs/root
-
-chown root:root /etc/ntp.conf
-chown root:root /etc/samba/smb.conf
-chown root:root /etc/vsftpd.conf
-
-chmod 644 /etc/ntp.conf
-chmod 644 /etc/samba/smb.conf
-chmod 644 /etc/vsftpd.conf
-chmod 777 /var/log/report.log
-
 sudo apt update -y
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -77,7 +45,7 @@ sleep 10;
 #sudo systemctl restart docker
 sleep 5;
 sudo usermod -aG docker $USER
-#service docker restart
+service docker restart
 sleep 10;
 sudo docker load -i iot6_ubuntu14.tar.gz
 sleep 5;
