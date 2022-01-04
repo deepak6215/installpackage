158bfedeb44c7aaef6a24e4af96d2791a50f1512
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-01-04 19:06:11 +0530

    Update create_dir_per_own.sh

--- create_dir_per_own.sh
+++ create_dir_per_own.sh
@@ -1,1 +1,33 @@
 
+
+mkdir -p /var/www/csvbackup
+mkdir -p /var/www/report/export/Scheduled_Report
+mkdir -p /var/www/iSolar/fetchDDT/csv
+mkdir -p /var/log/scaback_csv_influx_ingestor
+mkdir -p /var/log/prometheus
+mkdir -p /var/log/iGate_log
+mkdir -p /home/iplonshare/Scheduled_Report
+mkdir -p /home/iplonftp/csvbackup
+mkdir -p /home/iplonftp/Scheduled_Report
+
+adduser iplon www-data
+
+chown www-data:www-data -R /var/www/*
+chown www-data:www-data -R /var/www/.config/
+
+chmod 755 -R /var/www/*
+chmod 755 -R /var/www/.config/
+chmod 777 -R /var/www/report/export/Scheduled_Report/
+chmod 777 -R /var/www/iSolar/fetchDDT/csv
+
+chown root:crontab /var/spool/cron/crontabs/root
+chmod 600 /var/spool/cron/crontabs/root
+
+chown root:root /etc/ntp.conf
+chown root:root /etc/samba/smb.conf
+chown root:root /etc/vsftpd.conf
+
+chmod 644 /etc/ntp.conf
+chmod 644 /etc/samba/smb.conf
+chmod 644 /etc/vsftpd.conf
+chmod 777 /var/log/report.log
