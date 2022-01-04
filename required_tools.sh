eddf923e84cf96917d8ab5e7be47bb7db425ca53
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-01-04 19:29:08 +0530

    Update required_tools.sh

--- required_tools.sh
+++ required_tools.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-sudo apt-get install vim -y
-sudo apt-get install tcpdump -y
-sudo apt-get install screen -y
-sudo apt-get install unrar -y
-sudo apt-get install htop -y
-sudo apt-get install dos2unix -y
-sudo apt-get install expect -y
-sudo apt-get install ncdu -y
-sudo apt-get install sshpass -y
-sudo apt-get install net-tools -y
-sudo apt-get install nmap -y
-sudo apt-get install vsftpd -y
-sudo apt-get install openssh-server -y
-sudo apt-get install openssh-client -y
-sudo apt-get install ntp -y
-sudo apt install curl -y
+apt-get -qq update
+
+
+set -eu -o pipefail # fail on error , debug all lines
+
+sudo -n true
+test $? -eq 0 || exit 1 "you should have sudo priveledge to run this script"
+
+echo installing the must-have pre-requisites
+while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
+    vim
+    tcpdump
+    screen
+    unrar
+    htop
+    net-tools
+    sshpass
+    dos2unix
+    expect
+    openssh-server
+    openssh-client
+    openvpn
+    ntp
+    curl
+    apache2
+    nodejs
+    git
+    docker
+    python-pip
+    nmap
+    vsftpd
+EOF
+)
