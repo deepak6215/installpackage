ecbd7e98b598b2264832332ad32a1d4bd72442aa
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-01-04 15:45:29 +0530

    Update installPackage.sh

--- installPackage.sh
+++ installPackage.sh
@@ -63,9 +63,9 @@ apt update -y
 
 
 #ubuntu php5.5 docker container installation
-apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" -y
+#apt install apt-transport-https ca-certificates curl software-properties-common -y
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" -y
 
 #dwagent installation
 #wget https://www.dwservice.net/download/dwagent_x86.sh
