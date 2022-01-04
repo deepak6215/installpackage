71c3692f4501a2bc2a73643a1711a642b3f7e376
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-01-04 19:22:19 +0530

    Create required_tools.sh

---
+++ required_tools.sh
@@ -0,0 +1,16 @@
+sudo apt-get install vim -y
+sudo apt-get install tcpdump -y
+sudo apt-get install screen -y
+sudo apt-get install unrar -y
+sudo apt-get install htop -y
+sudo apt-get install dos2unix -y
+sudo apt-get install expect -y
+sudo apt-get install ncdu -y
+sudo apt-get install sshpass -y
+sudo apt-get install net-tools -y
+sudo apt-get install nmap -y
+sudo apt-get install vsftpd -y
+sudo apt-get install openssh-server -y
+sudo apt-get install openssh-client -y
+sudo apt-get install ntp -y
+sudo apt install curl -y
