b373d5c24deb288d3cc8b71037c48bba6efbab57
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-01-04 18:26:54 +0530

    Update installPackage.sh

--- installPackage.sh
+++ installPackage.sh
@@ -76,16 +76,6 @@ apt update -y
 # Install Nodejs Package
 installPackage nodejs
 
-mkdir -p /var/www/csvbackup
-mkdir -p /var/www/report/export/Scheduled_Report
-mkdir -p /var/www/iSolar/fetchDDT/csv
-mkdir -p /var/log/scaback_csv_influx_ingestor
-mkdir -p /var/log/prometheus
-mkdir -p /var/log/iGate_log
-mkdir -p /home/iplonshare/Scheduled_Report
-mkdir -p /home/iplonftp/csvbackup
-mkdir -p /home/iplonftp/Scheduled_Report
-
 #mkdir -p /home/iplonshare/csvbackup
 #chown -R iplonshare:shareaccess /home/iplonshare/csvbackup/
 chown -R iplonshare:shareaccess /home/iplonshare/Scheduled_Report
@@ -129,27 +119,6 @@ cd $SCRIPT_DIR
 
 apt update -y 
 
-adduser iplon www-data
-
-chown www-data:www-data -R /var/www/*
-chown www-data:www-data -R /var/www/.config/
-
-chmod 755 -R /var/www/*
-chmod 755 -R /var/www/.config/
-chmod 777 -R /var/www/report/export/Scheduled_Report/
-chmod 777 -R /var/www/iSolar/fetchDDT/csv
-
-chown root:crontab /var/spool/cron/crontabs/root
-chmod 600 /var/spool/cron/crontabs/root
-
-chown root:root /etc/ntp.conf
-chown root:root /etc/samba/smb.conf
-chown root:root /etc/vsftpd.conf
-
-chmod 644 /etc/ntp.conf
-chmod 644 /etc/samba/smb.conf
-chmod 644 /etc/vsftpd.conf
-chmod 777 /var/log/report.log
 
 #mysql -u root -p$MYSQL_PASS -e "CREATE DATABASE IF NOT EXISTS $DB"
 
