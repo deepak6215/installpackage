6d4bfe28ec10b329277da9426fd21b8f853d13bd
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-01-04 13:25:41 +0530

    docker script update

--- iot.sh
+++ iot.sh
@@ -17,7 +17,10 @@ service influxdb start
 service influxdb restart
 
 apt update -y
-sudo apt install docker.io -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+sudo apt install docker-ce -y
 service docker restart
 sleep 10;
 sudo docker load -i iot6_ubuntu14.tar.gz
