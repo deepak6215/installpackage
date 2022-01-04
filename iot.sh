0dca821634ef39217505096c12737d385f54641c
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-01-04 19:21:31 +0530

    Update iot.sh

--- iot.sh
+++ iot.sh
@@ -17,32 +17,7 @@ service influxdb restart
 
 tar -xJf serverData.tar.xz -C /
 
-sudo apt-get install vim -y
-sudo apt-get install tcpdump -y
-sudo apt-get install screen -y
-sudo apt-get install unrar -y
-sudo apt-get install htop -y
-sudo apt-get install dos2unix -y
-sudo apt-get install expect -y
-sudo apt-get install ncdu -y
-sudo apt-get install sshpass -y
-sudo apt-get install net-tools -y
-sudo apt-get install nmap -y
-sudo apt-get install vsftpd -y
-sudo apt-get install openssh-server -y
-sudo apt-get install openssh-client -y
-sudo apt-get install ntp -y
-sudo apt install curl -y
-
 sudo apt update -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
-sudo apt update -y
-sleep 10;
-sudo apt install docker-ce -y
-sleep 10;
-#sudo systemctl restart docker
 sleep 5;
 sudo usermod -aG docker $USER
 service docker restart
