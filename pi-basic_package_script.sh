7ca235d95070f40e025eaeb03c682959207e2529
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-01-05 16:57:23 +0530

    Update pi-basic_package_script.sh

--- pi-basic_package_script.sh
+++ pi-basic_package_script.sh
@@ -3,10 +3,8 @@ sudo apt-get install -y adduser libfontconfig1
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise_7.0.6_arm64.deb
 sudo dpkg -i grafana-enterprise_7.0.6_arm64.deb
 service grafana-server restart
-service grafana-server status
 wget https://dl.influxdata.com/influxdb/releases/influxdb_1.8.5_arm64.deb
 sudo dpkg -i influxdb_1.8.5_arm64.deb
 service influxdb restart
-service influxdb status
-sudo apt install docker.io
+sudo apt install docker.io -y
 sudo usermod -aG docker ubuntu
